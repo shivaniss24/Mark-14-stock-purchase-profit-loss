740abc3ba8f121fd0e56031b3fcfd583025e3ef9
Shivani Soni <eng.shivanisoni2405@gmail.com>
2022-09-13 18:25:16 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+//step-1 = selecting all the elements
+
+var initialPrice=document.querySelector("#initial-price");
+var stocksPrice=document.querySelector("#stocks-price");
+var currentPrice=document.querySelector("#current-price");
+var submitButton=document.querySelector("#submit-btn");
+var outputBox=document.querySelector("#output-box");
+
+
+function calculateProfitAndLoss(initial,quantity,current)
+{
+    if(initial>current)
+    {
+        
+    }
+}
+
+
+
+submitButton.addEventListener('click',);

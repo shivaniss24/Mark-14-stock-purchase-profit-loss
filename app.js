6caace4b2d2faabbfb464244f18828b64dd853c2
Shivani Soni <eng.shivanisoni2405@gmail.com>
2022-09-13 19:59:33 +0530

    update js,html with profit loss

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //step-1 = selecting all the elements
 
 var initialPrice=document.querySelector("#initial-price");
-var stocksPrice=document.querySelector("#stocks-price");
+var stocksQuantity=document.querySelector("#stocks-quantity");
 var currentPrice=document.querySelector("#current-price");
 var submitButton=document.querySelector("#submit-btn");
 var outputBox=document.querySelector("#output-box");
@@ -9,12 +9,53 @@ var outputBox=document.querySelector("#output-box");
 
 function calculateProfitAndLoss(initial,quantity,current)
 {
+    
     if(initial>current)
     {
-        
+        //loss logic here
+        var loss=(initial-current) *quantity;
+        var lossPercentage=(loss/initial)*100;
+
+        outputBox.innerText="Your loss is "+ loss + " and loss percentage is "+lossPercentage.toFixed(2)+ " % 👎 ";
+        outputBox.style.color='green';
     }
+    if(current>initial)
+    {
+        //profit logic here
+        var profit=(current-initial)*quantity;
+        var profitPercentage=(profit/initial)*100;
+        outputBox.innerText="Your profit is "+ profit + " and profit percentage is "+profitPercentage.toFixed(2)+ " % 👍 ";
+        outputBox.style.color='green';
+        outputBox.style.padding='2rem';
+        outputBox.style.fontsize ='bold';
+    }
+    else if(initialPrice.value==='',currentPrice.value==='',stocksQuantity.value==='')
+    {
+        //rest of logic
+        outputBox.innerText="oh oh! please enter valid values 🖊️ ";
+        outputBox.style.color='green';
+        outputBox.style.padding='2rem';
+
+}
+else{
+    outputBox.innerText="No pain no gain and no gain no pain. 🙌  ";
+    outputBox.style.color='green';
+    outputBox.style.padding='2rem';
+}
+}
+
+function submitHandler()
+{
+    var ip= Number(initialPrice.value);
+    var qty=Number(stocksQuantity.value);
+    var curr=Number(currentPrice.value);
+
+    calculateProfitAndLoss(ip,qty,curr);
+
+
 }
 
 
 
-submitButton.addEventListener('click',);
+
+submitButton.addEventListener('click',submitHandler);
